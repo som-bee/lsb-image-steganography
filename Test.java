b231d11bcb109676681ac96065d09c8f324100b9
Somnath Bera <somnathbera1919@gmail.com>
2023-03-05 01:04:45 +0530

    test program updated

--- Test.java
+++ Test.java
@@ -1,23 +1,10 @@
 import java.util.Arrays;
 
+
+// for testing purposes
 public  class Test {
-  
-// function have implementation of another 
-    // function inside local class
-    static void Foo()
-    {
-        int x=20;
-        // local class
-        class Local {
-            void fun()
-            {
-               // x++;
-                System.out.println("geeksforgeeks");
-            }
-        }
-        new Local().fun();
-            System.out.println(x);
-    }
+
+
     public static void main(String[] args)
     {
         Boolean[][] colorsArray = new Boolean[3][8];
